373b055325f0a40ebf489189842139bbc0187626
Nick <nicolas.theo.frey@gmail.com>
2023-09-20 09:30:31 +0200

    init

--- fse_all/src/main/java/net/microwonk/fse1/aufg1/Druckbar.java
+++
@@ -1,8 +0,0 @@
-package net.microwonk.fse1.aufg1;
-
-// ein sealed interface muss eine Reihe von Klassen oder Interfaces permitten
-// , damit Pattern matching ohne default case und andere Sachen möglich sind.
-// (ist auch besser, falls das Interface außerhalb der Dedizierten Liste an Klassen nicht verwendet werden muss
-public sealed interface Druckbar permits Namensschilderdruck, Rahmendruck, Standarddruck {
-    void drucken(Person person);
-}
--- fse_all/src/main/java/net/microwonk/fse1/interfaces/IExport.java
+++
@@ -1,8 +0,0 @@
-package net.microwonk.fse1.interfaces;
-
-import java.util.Collection;
-
-public interface IExport {
-    <T>String export(Collection<T> data);
-
-}
